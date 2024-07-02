224469f71078f36c28df2c40e96c07ba2a90059a
biewang.hjj <942101897@qq.com>
2024-07-02 10:05:31 +0800

    feat(配置页、渲染页): 配置页配置文件，渲染页读取yml

--- src/pages/Config/index.tsx
+++ src/pages/Config/index.tsx
@@ -2,31 +2,17 @@ import {
   PageContainer,
   ProCard,
   ProDescriptions,
-  // ProForm,
-  // ProFormSwitch,
-  ProFormInstance,
   ProFormSegmented,
   ProFormText,
-  // ProFormSelect,
-  // ProFormGroup,
   StepsForm,
 } from '@ant-design/pro-components';
-// import { Button, message } from 'antd';
-// import {useLocation} from 'umi';
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
+import styles from './index.less';
 
-const ConfigPanel: React.FC<{ baseUrl: string; handleWebUrlChange: any }> = ({
-  baseUrl,
-  handleWebUrlChange,
-}) => {
-  const formRef = useRef<ProFormInstance>();
-  const configFilePath = `${baseUrl}api/v1/config`;
-  const renderUrl = `${baseUrl}/render`;
-  // useEffect(() => {
-  //   // 如果需要在组件挂载时初始化输入框，可以在这里调用一次onTextChange
-  //   // 注意：这取决于你的业务需求，通常情况下不需要这样做
-  //   handleWebUrlChange(configFilePath);
-  // }, []);
+const ConfigPanel: React.FC<{
+  configFilePath: string;
+  handleChangeWebUrl: any;
+}> = ({ configFilePath, handleChangeWebUrl }) => {
   return (
     <div style={{ width: '100%' }}>
       <ProFormSegmented
@@ -44,7 +30,6 @@ const ConfigPanel: React.FC<{ baseUrl: string; handleWebUrlChange: any }> = ({
         stepsProps={{
           size: 'small',
         }}
-        formRef={formRef}
         submitter={{
           render: () => {
             return <></>;
@@ -67,9 +52,7 @@ const ConfigPanel: React.FC<{ baseUrl: string; handleWebUrlChange: any }> = ({
             tooltip="需要确保url的总长不超过浏览器限制"
             placeholder="请输入配置文件地址"
             value={configFilePath}
-            onChange={() => {
-              handleWebUrlChange(`${renderUrl}?config=${configFilePath}`);
-            }}
+            onChange={handleChangeWebUrl}
           />
         </StepsForm.StepForm>
       </StepsForm>
@@ -170,44 +153,29 @@ const ConfigPanel: React.FC<{ baseUrl: string; handleWebUrlChange: any }> = ({
 };
 
 const WebBrowserComponent: React.FC<{ webUrl: string }> = ({ webUrl }) => {
-  // const [url, setUrl] = useState(
-  //   weburl,
-  // ); // 初始URL
-
-  // const handleInputChange = (e) => {
-  //   // 更新URL状态，注意需要做一些基本的安全检查，比如阻止JavaScript注入等
-  //   const newUrl = e.target.value;
-  //   setUrl(newUrl);
-  // };
-
   return (
     <ProDescriptions
       title="模拟浏览"
-      // request={async () => {
-      //   return Promise.resolve({
-      //     success: true,
-      //     data: {
-      //       url: webUrl,
-      //     },
-      //   });
-      // }}
       column={1}
       columns={[
         {
           title: '地址',
           key: 'text',
-          dataIndex: webUrl,
+          dataIndex: 'webUrl',
           copyable: true,
           ellipsis: true,
         },
       ]}
+      dataSource={{
+        webUrl: webUrl,
+      }}
     >
       <iframe
+        className={styles.iframe}
         title="web-browser-component"
         src={webUrl}
         width="100%"
         height="400vh"
-        frameBorder="0"
         sandbox="allow-same-origin allow-scripts allow-popups"
       />
     </ProDescriptions>
@@ -215,24 +183,33 @@ const WebBrowserComponent: React.FC<{ webUrl: string }> = ({ webUrl }) => {
 };
 
 const ConfigPage: React.FC = () => {
-  const [baseUrl] = useState(
-    `${window.location.origin}${window.location.pathname.substring(
-      0,
-      window.location.pathname.indexOf('config'),
-    )}`,
+  // 计算兼容publicPath
+  const baseUrl = `${
+    window.location.origin
+  }${window.location.pathname.substring(
+    0,
+    window.location.pathname.indexOf('config'),
+  )}`;
+
+  const renderUrl = `${baseUrl}render`;
+
+  //设置传递状态
+  const [configFilePath] = useState<string>(`${baseUrl}api/v1/config`);
+  const [webUrl, setWeburl] = useState<string>(
+    `${renderUrl}?config=${configFilePath}`,
   );
-  const [webUrl, setWeburl] = useState<string>('');
-  const handleWebUrlChange = (text: string) => {
-    setWeburl(text);
+  const handleChangeWebUrl = (e: { currentTarget: any }) => {
+    setWeburl(`${renderUrl}?config=${e.currentTarget.value}`);
   };
+
   return (
     <PageContainer ghost>
       <ProCard direction="column" ghost>
         <ProCard gutter={16} ghost style={{ height: 300 }} layout="center" wrap>
           <ProCard colSpan={{ md: 6 }} layout="center">
             <ConfigPanel
-              baseUrl={baseUrl}
-              handleWebUrlChange={handleWebUrlChange}
+              configFilePath={configFilePath}
+              handleChangeWebUrl={handleChangeWebUrl}
             />
           </ProCard>
           <ProCard colSpan={{ md: 18 }}>
--- src/pages/Render/index.tsx
+++ src/pages/Render/index.tsx
@@ -1,6 +1,41 @@
 import { PageContainer, ProCard } from '@ant-design/pro-components';
+import YAML from 'js-yaml';
+import { useEffect, useState } from 'react';
 
 export default () => {
+  const [yamlData, setYamlData] = useState(null);
+
+  useEffect(() => {
+    async function fetchYamlContent() {
+      const searchParams = new URLSearchParams(window.location.search);
+      const configFileUrl = searchParams.get('config');
+      if (!configFileUrl) return;
+      try {
+        console.log(configFileUrl);
+        const response = await fetch(configFileUrl);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.text();
+        try {
+          // 使用js-yaml的safeLoad或safeLoadAll方法解析YAML字符串
+          const parsedYaml = YAML.load(data);
+          setYamlData(parsedYaml);
+        } catch (e) {
+          console.error('Error parsing YAML:', e);
+        }
+      } catch (error) {
+        console.error('Error fetching YAML content:', error);
+      }
+    }
+
+    fetchYamlContent();
+  }, []); // 空数组作为依赖，确保只在组件挂载时运行一次
+
+  if (!yamlData) return <div>Loading...</div>;
+
+  console.log(yamlData);
+
   return (
     <div
       style={{
@@ -16,8 +51,19 @@ export default () => {
             layout="center"
             wrap
           >
-            <ProCard colSpan={{ md: 4 }} layout="center"></ProCard>
-            <ProCard colSpan={{ md: 8 }} />
+            <ProCard
+              colSpan={{ md: yamlData.showImage ? 4 : 24 }}
+              layout="center"
+            ></ProCard>
+            {yamlData ? (
+              yamlData.showImage ? (
+                <ProCard colSpan={{ md: 8 }} />
+              ) : (
+                <></>
+              )
+            ) : (
+              <></>
+            )}
           </ProCard>
         </ProCard>
       </PageContainer>
