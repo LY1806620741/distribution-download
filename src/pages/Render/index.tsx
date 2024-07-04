122c0f435cf760753b12d2ec549a4c162e1cb27f
biewang.hjj <942101897@qq.com>
2024-07-04 22:34:13 +0800

    feat(渲染页 仿vscode风格): 仿vscode风格 part2

--- src/pages/Render/index.tsx
+++ src/pages/Render/index.tsx
@@ -1,16 +1,18 @@
 import { DownOutlined } from '@ant-design/icons';
 import { PageContainer, ProCard } from '@ant-design/pro-components';
-import { Dropdown, Flex } from 'antd';
+import { Dropdown, Flex, Layout, Menu, MenuProps, Row } from 'antd';
 import YAML from 'js-yaml';
 import { useEffect, useState } from 'react';
-// import styles from './index.less';
+import styles from './index.less';
+
+const { Header, Content, Footer } = Layout;
 
 export default () => {
   const [yamlData, setYamlData] = useState(null);
 
   useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
     async function fetchYamlContent() {
-      const searchParams = new URLSearchParams(window.location.search);
       const configFileUrl = searchParams.get('config');
       if (!configFileUrl) return;
       try {
@@ -37,54 +39,93 @@ export default () => {
 
   if (!yamlData) return <div>Loading...</div>;
 
-  console.log(yamlData);
+  const items1: MenuProps['items'] = ['1', '2', '3'].map((key) => ({
+    key,
+    label: `nav ${key}`,
+  }));
 
   return (
-    <div
-      style={{
-        background: '#F5F7FA',
-      }}
-    >
-      <PageContainer>
-        <ProCard direction="column" ghost>
-          <ProCard
-            gutter={16}
-            ghost
-            style={{ height: 300 }}
-            layout="center"
-            wrap
-          >
-            <ProCard
-              colSpan={{ md: yamlData.showImage ? 4 : 24 }}
-              layout="center"
-              split={'horizontal'}
-            >
-              {yamlData.label && <div>{yamlData.label}</div>}
-              {yamlData.desciption && <div>{yamlData.desciption}</div>}
-              {yamlData.download && (
-                <div>
-                  {' '}
-                  <Dropdown.Button size="large" icon={<DownOutlined />}>
-                    <Flex vertical>
-                      <span>Download Mac Universal</span>
-                      <small>Stable Build</small>
-                    </Flex>
-                  </Dropdown.Button>
-                </div>
-              )}
-              {yamlData.otherDownload && (
-                <div>
-                  <div
-                    dangerouslySetInnerHTML={{ __html: yamlData.otherDownload }}
-                  />
-                </div>
-              )}
-              {yamlData.liscense && <div>{yamlData.liscense}</div>}
+    <>
+      <Layout>
+        <Header className={styles.layoutHeader}>
+          <a href={window.location.pathname + window.location.search}>
+            {yamlData.icon && <img src={yamlData.icon} height="40" />}
+            <span>Visual Studio Code</span>
+          </a>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            items={items1}
+            style={{ flex: 1, minWidth: 0 }}
+          ></Menu>
+        </Header>
+        <Content>
+          <Row className={styles.tips}>
+            <p>
+              <a href="/updates">Version 1.91</a> is now available! Read about
+              the new features and fixes from June.
+            </p>
+          </Row>
+          <PageContainer className={styles.mainContainer}>
+            <ProCard direction="column" ghost>
+              <ProCard
+                gutter={16}
+                ghost
+                style={{ height: 300 }}
+                layout="center"
+                wrap
+              >
+                <ProCard
+                  colSpan={{ md: yamlData.showImage ? 4 : 24 }}
+                  layout="center"
+                  split={'horizontal'}
+                  className={styles.downloadCard}
+                >
+                  {yamlData.label && (
+                    <div style={{ margin: '20px 0 10px 0' }}>
+                      <h1>
+                        {yamlData.label}
+                        {yamlData.labelStrong && (
+                          <strong style={{ display: 'block' }}>
+                            {yamlData.labelStrong}
+                          </strong>
+                        )}
+                      </h1>
+                    </div>
+                  )}
+                  {yamlData.desciption && <div>{yamlData.desciption}</div>}
+                  {yamlData.download && (
+                    <div>
+                      {' '}
+                      <Dropdown.Button size="large" icon={<DownOutlined />}>
+                        <Flex vertical>
+                          <span>Download Mac Universal</span>
+                          <small>Stable Build</small>
+                        </Flex>
+                      </Dropdown.Button>
+                    </div>
+                  )}
+                  {yamlData.otherDownload && (
+                    <div>
+                      <div
+                        dangerouslySetInnerHTML={{
+                          __html: yamlData.otherDownload,
+                        }}
+                      />
+                    </div>
+                  )}
+                  {yamlData.liscense && <div>{yamlData.liscense}</div>}
+                </ProCard>
+                {yamlData?.showImage && <ProCard colSpan={{ md: 8 }} />}
+              </ProCard>
             </ProCard>
-            {yamlData?.showImage && <ProCard colSpan={{ md: 8 }} />}
-          </ProCard>
-        </ProCard>
-      </PageContainer>
-    </div>
+          </PageContainer>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </>
   );
 };
